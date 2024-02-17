1198484b0ddecc137b09d94b3c277dcf0533f5f2
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2024-02-17 13:48:54 +0600

    added bcrypt for hashing password

--- src/controllers/registration-controller.js
+++ src/controllers/registration-controller.js
@@ -1,13 +1,15 @@
 const { StatusCodes } = require('http-status-codes')
+const bcrypt=require('bcrypt');
 const {registrationService}=require('../services')
 const { successResponse,errorResponse } = require('../utils/common')
 
 async function createUser(req,res){
+  const hashpassword=await bcrypt.hash(req.body.password,10);
   try {
     const user=await registrationService.createUser({
       username:req.body.username,
       email:req.body.email,
-      password:req.body.password
+      password:hashpassword
     })
     successResponse.data=user;
     successResponse.message='Successfully registration complete';
