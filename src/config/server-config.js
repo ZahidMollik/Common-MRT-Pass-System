bd300dca76fa7171c4213a582d1f8638e2f4335c
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2024-03-17 16:39:05 +0600

    server-config modified by removing db env variables

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,12 +1,6 @@
 const dotenv=require('dotenv');
-const config=require('./config.json');
 dotenv.config();
 
-const developmentConfig = config.development;
-developmentConfig.username = process.env.DB_USERNAME;
-developmentConfig.password = process.env.DB_PASSWORD;
-developmentConfig.database = process.env.DB_NAME;
-developmentConfig.host = process.env.DB_HOST;
 
 module.exports={
   PORT:process.env.PORT
