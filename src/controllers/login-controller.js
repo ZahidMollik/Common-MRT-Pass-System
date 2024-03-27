cae3ed4d6f731ce2b3394e0ff1b76b0ec54a8eac
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2024-03-28 00:57:38 +0600

    added sameSite for cross-origin requests

--- src/controllers/login-controller.js
+++ src/controllers/login-controller.js
@@ -14,7 +14,7 @@ async function loginUser(req,res){
       email:user.email
     }
     const token=jwt.sign(payload,process.env.SECRETKEY);
-    res.cookie('jwt',token,{secure: true,httpOnly:true,maxAge:100000000})
+    res.cookie('jwt',token,{secure: true,httpOnly:true,maxAge:100000000,sameSite:'None'})
     successResponse.message='Login Successful';
     return res.status(StatusCodes.OK)
               .json(successResponse);
